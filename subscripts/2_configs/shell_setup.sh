fa069b435fa2127e9363e745c6d959c635aac920
Anders HL Lauridsen <anders.hl.lauridsen@gmail.com>
2024-06-09 21:34:33 +0200

    .

--- subscripts/2_configs/shell_setup.sh
+++ subscripts/2_configs/shell_setup.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 chsh -s /bin/zsh
 homectl update --shell=/bin/zsh anders
+
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
 cd Git
 echo "source ${(q-)PWD}/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc
 cd ~/Git/catpuccin-syntax-highlight-zsh/themes/
 cp -v catppuccin_mocha-zsh-syntax-highlighting.zsh ~/.zsh/
 echo "source ~/.zsh/catppuccin_mocha-zsh-syntax-highlighting.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc
+
 echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.zshrc
 echo '[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.zshrc
 echo 'eval "$(pyenv init -)"' >> ~/.zshrc
+
 echo 'source <(fzf --zsh)'
