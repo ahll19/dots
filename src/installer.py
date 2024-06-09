a8f052fcfbc32e6ff01d740683c227e53e4be14b
Anders HL Lauridsen <anders.hl.lauridsen@gmail.com>
2024-06-09 14:15:18 +0200

    .

--- src/installer.py
+++ src/installer.py
@@ -4,9 +4,10 @@ from typing import List
 
 class Installer:
     @classmethod
-    def _validate_packages_in_ar(cls, packages: List[str]) -> int:
+    def _validate_packages_in_ar(cls, packages: List[str]) -> List[int]:
+        exit_codes = []
         for package in packages:
-            exit_code = os.system("pacman -Ss " + package)
+            exit_codes.append(os.system("pacman -Ss " + package))
 
 if __name__ == "__main__":
     print(Installer._validate_packages_in_ar(["vlc", "firefox"]))
