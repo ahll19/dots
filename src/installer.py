19db33a406eed80c72e646da865fe9d58b2dc2c5
Anders HL Lauridsen <anders.hl.lauridsen@gmail.com>
2024-06-09 14:00:30 +0200

    .

---
+++ src/installer.py
@@ -0,0 +1,16 @@
+import os
+from typing import List
+
+
+class Installer:
+    @classmethod
+    def _validate_packages_in_ar(cls, packages: List[str]) -> int:
+        package_str = ""
+        for package in packages:
+            package_str += f"{package} "
+        
+        return os.system("pacman -Ss " + package_str)
+
+
+if __name__ == "__main__":
+    Installer._validate_packages_in_ar(["vlc", "firefox"])
