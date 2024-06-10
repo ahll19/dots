c009659a3341cb83922a0a4db3150187eef4a8af
Anders Lauridsen <ahll19@student.aau.dk>
2024-06-10 11:13:20 +0200

    whoops

--- setup.sh
+++ setup.sh
@@ -9,7 +9,7 @@ bash /home/anders/Git/dots/subscripts/0_setup/setup.sh
 
 bash /home/anders/Git/dots/subscripts/1_installs/desktop_environment.sh
 bash /home/anders/Git/dots/subscripts/1_installs/misc_software.sh
-bash /home/anders/Git/dots/subscripts/1_installs/software_dev.sh1
+bash /home/anders/Git/dots/subscripts/1_installs/software_dev.sh
 
 bash /home/anders/Git/dots/subscripts/2_configs/git_repos.sh
 bash /home/anders/Git/dots/subscripts/2_configs/shell_setup.sh
