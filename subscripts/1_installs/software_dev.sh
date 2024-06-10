4d82e3982d7a89e9ba3e118e7a618e4cc6615c38
Anders Lauridsen <ahll19@student.aau.dk>
2024-06-10 11:03:05 +0200

    .

--- subscripts/1_installs/software_dev.sh
+++ subscripts/1_installs/software_dev.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-sudo pacman -S zellij gitui stow bat fzf zsh rustup pyenv neovim wl-clipboard --noconfirm
+sudo pacman -S zsh zellij gitui stow bat fzf zsh rustup pyenv neovim wl-clipboard --noconfirm
 yay -S visual-studio-code-bin pyenv-virtualenv --noconfirm
 rustup default stable
