48e1cc81293c6eb727fef54b13a32bd7475e46ca
Anders Lauridsen <55282079+ahll19@users.noreply.github.com>
2024-06-14 09:44:37 +0200

    Update software_dev.sh

--- subscripts/1_installs/software_dev.sh
+++ subscripts/1_installs/software_dev.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-sudo pacman -S zsh zellij gitui stow bat fzf zsh rustup pyenv neovim wl-clipboard --noconfirm
+sudo pacman -S zsh zellij gitui stow bat fzf zsh rustup pyenv neovim wl-clipboard zsh --noconfirm
 yay -S visual-studio-code-bin pyenv-virtualenv --noconfirm
 rustup default stable
